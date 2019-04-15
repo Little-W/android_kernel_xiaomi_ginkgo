1681a1ccf9cb94173c6c644db756d6bc343c3736
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-14 20:19:37 -0700

    加入头文件库

---
+++ 1.sh
@@ -0,0 +1,12 @@
+mkdir out
+export ARCH=arm64
+export SUBARCH=arm64
+export DTC_EXT=dtc
+make O=out ARCH=arm64 vendor/ginkgo-perf_defconfig
+PATH="${PWD}/bin:${PWD}/toolchain/bin:/toolchain/arm-eabi-gcc/bin:${PATH}" \
+make -j$(nproc --all) O=out -o3 \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+                      CROSS_COMPILE=aarch64-linux-gnu- | tee kernel.log
