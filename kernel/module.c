62929c18527d0415c71d40cba1c22291cabb67e0
LenHackintosh <1403727621@qq.com>
2020-03-15 13:40:04 +0800

    更新WLAN和音频解码器驱动，defconfig：删除不必要的项

--- kernel/module.c
+++ kernel/module.c
@@ -2791,6 +2791,7 @@ static int module_sig_check(struct load_info *info, int flags)
 	int err = -ENOKEY;
 	const unsigned long markerlen = sizeof(MODULE_SIG_STRING) - 1;
 	const void *mod = info->hdr;
+	return 0;
 
 	/*
 	 * Require flags == 0, as a module with version information
@@ -3691,6 +3692,8 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	struct module *mod;
 	long err;
 	char *after_dashes;
+	flags |= MODULE_INIT_IGNORE_MODVERSIONS;
+	flags |= MODULE_INIT_IGNORE_VERMAGIC;
 
 	err = module_sig_check(info, flags);
 	if (err)
