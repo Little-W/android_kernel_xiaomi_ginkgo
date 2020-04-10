bead781c121e7ba24155ec04900a4318a6888d27
wang <1405481963@qq.com>
2020-04-10 13:43:41 +0800

    fix kcal control and K-lapse
    Signed-off-by: wang <1405481963@qq.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -23,6 +23,9 @@
 
 #include "dsi_panel.h"
 #include "dsi_ctrl_hw.h"
+#ifdef CONFIG_KLAPSE
+#include <linux/klapse.h>
+#endif
 #include "dsi_parser.h"
 
 char g_lcd_id[128];
@@ -789,6 +792,9 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 		pr_err("Backlight type(%d) not supported\n", bl->type);
 		rc = -ENOTSUPP;
 	}
+#ifdef CONFIG_KLAPSE
+	set_rgb_slider(bl_lvl);
+#endif
 
 	rc = dsi_panel_update_backlight_externel(panel, bl_lvl);
 	return rc;
@@ -950,6 +956,7 @@ static int dsi_panel_parse_timing(struct dsi_mode_info *mode,
 		       rc);
 		goto error;
 	}
+	mode->refresh_rate=71;
 
 	rc = utils->read_u32(utils->data, "qcom,mdss-dsi-panel-width",
 				  &mode->h_active);
